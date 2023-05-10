49d01eb64b95c28eaf7212e1ef5e93da439e4572
javierquinterocl <jaquinterocl@ufpso.edu.co>
2023-05-10 17:02:34 -0500

    js aprendiendo

---
+++ js/clase.js
@@ -0,0 +1,42 @@
+// variables
+const nameuser = "Javier";
+
+var x = "valor";
+let y = "valor";
+let a = 20;
+let b = 2.3;
+let string = "Mi nombre es Javier"
+let number = "0120232"
+let booleano = false;
+let booleanouno = true;
+let array = [1,2];
+let array_string = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"];
+
+// OPERADORES ARITMETICOS   : + - / * %
+let SUMA = 1 +5+a;
+let resta = 1-5-a;
+let multi = 1 * 5 * 4;
+let divi = 1/ 5 / a;
+let module = 10%2;
+let elev = 5^2;
+console.log("-"+SUMA+"\n- " + resta+"\n -" + multi+"\n-"+divi +"\n"+" \n "+ module+ " \n"+  elev+" \n") ;
+
+//operadores de comparacion = / == / == / <> / >= / > / <
+//OPERADORES LOGICOS and && / or || /
+
+if(10>5){
+    for(let i = 0; i <array_string.length;i++){
+       console.log (array_string[i]);
+
+    }
+}
+let j= 5;
+let k = 10;
+while(k>=j){
+    console.log("Entra")
+    j++;
+    break
+
+}
+//Eventos Leer
+//metodos JS
